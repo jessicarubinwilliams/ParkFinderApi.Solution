454c10481e98d6890e7cc20b6862d9d03eeedf20
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-11-07 14:36:40 -0800

    Add action method for updating an animal

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using CretaceousPark.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CretaceousPark.Controllers
@@ -46,6 +47,41 @@ namespace CretaceousPark.Controllers
       }
 
       return animal;
+    }
+
+    //PUT api/Animals/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
     }
   }
 }
